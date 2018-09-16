38de9a8a9d4551b66948a17798556a730c6763bf
Panca Sutresna <panca.sutresna@gmail.com>
2018-09-16 23:06:12 +0700

    ignore node_modules and build folders

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Grid, Navbar, Jumbotron, Button, Alert} from 'react-bootstrap';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar inverse fixedTop>
+          <Grid>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="/">React App</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+          </Grid>
+        </Navbar>
+        <Jumbotron>
+          <Grid>
+            <h1>Welcome to React</h1>
+            <Alert bsStyle="warning">
+              <strong>Holy guacamole!</strong> Best check yo self, you're not looking too
+              good.
+            </Alert>
+            <p>
+              <Button
+                bsStyle="success"
+                bsSize="large"
+                href="http://react-bootstrap.github.io/components.html"
+                target="_blank">
+                View React Bootstrap Docs
+              </Button>
+            </p>
+          </Grid>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
+
+export default App;
